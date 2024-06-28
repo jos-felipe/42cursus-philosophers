c7bdba2abf712ee5eeb2c0556e74120b43147a92
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-06-28 09:29:55 -0300

    memory check

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 16:11:26 by josfelip          #+#    #+#             */
-/*   Updated: 2024/06/26 13:51:39 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/06/28 09:24:35 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(void)
 {
 	char	*str;
-	
+
 	str = ft_strdup("Hello World!\n");
 	ft_collect_mem(str);
 	new_function(str);
