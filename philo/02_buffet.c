5fcb6488c8abae3220fb199625ad2a58ac11fa2e
José Felipe <correio@josefelipe.eng.br>
2024-07-11 14:41:37 -0300

    manage the threads

--- philo/02_buffet.c
+++ philo/02_buffet.c
@@ -6,38 +6,38 @@
 /*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 12:06:28 by josfelip          #+#    #+#             */
-/*   Updated: 2024/07/11 13:55:08 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/07/11 14:29:20 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	*philo_take_a_seat(void *arguments)
-{
-	t_diner			*philo;
+// static void	*philo_take_a_seat(void *arguments)
+// {
+// 	t_diner			*philo;
 
-	philo = (t_diner *)arguments;
-	printf("P%u: Hi, I'm Philosopher %u!\n", philo->diner_id, philo->diner_id);
-	printf("P%u: I'll die if I wait more than %u ms to eat.\n", \
-	philo->diner_id, philo->diet[TIME_TO_DIE]);
-	printf("P%u: I take %u ms to eat.\n", philo->diner_id, \
-	philo->diet[TIME_TO_EAT]);
-	printf("P%u: Then I sleep for %u ms.\n", philo->diner_id, \
-	philo->diet[TIME_TO_SLEEP]);
-	printf("P%u: I'll take %u meals before I leave the table.\n", \
-	philo->diner_id, philo->diet[MEALS]);
-	return (NULL);
-}
+// 	philo = (t_diner *)arguments;
+// 	printf("P%u: Hi, I'm Philosopher %u!\n", philo->diner_id, philo->diner_id);
+// 	printf("P%u: I'll die if I wait more than %u ms to eat.\n", \
+// 	philo->diner_id, philo->diet[TIME_TO_DIE]);
+// 	printf("P%u: I take %u ms to eat.\n", philo->diner_id, \
+// 	philo->diet[TIME_TO_EAT]);
+// 	printf("P%u: Then I sleep for %u ms.\n", philo->diner_id, \
+// 	philo->diet[TIME_TO_SLEEP]);
+// 	printf("P%u: I'll take %u meals before I leave the table.\n", \
+// 	philo->diner_id, philo->diet[MEALS]);
+// 	return (NULL);
+// }
 
 static void	philo_set_diner_diet(t_diner *philo, \
 unsigned int *args)
 {
 	unsigned int	u;
 
-	u = 1;
+	u = 0;
 	while (u < N_ARGS)
 	{
-		philo->diet[u - 1] = args[u];
+		philo->diet[u] = args[u];
 		u++;
 	}
 }
@@ -70,7 +70,7 @@ unsigned int *args)
 		host->forks[u] = 1;
 		host->list_of_diners[u].forks = host->forks;
 		result_code = pthread_create(&host->list_of_diners[u].diner, \
-		NULL, philo_take_a_seat, &host->list_of_diners[u]);
+		NULL, philo_manage_the_forks, &host->list_of_diners[u]);
 		assert(!result_code);
 		u++;
 	}
---
+++ philo/03_start_the_meal.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   03_start_the_meal.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:06:28 by josfelip          #+#    #+#             */
+/*   Updated: 2024/07/11 14:38:10 by josfelip         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	*philo_manage_the_forks(void *arguments)
+{
+	t_diner			*philo;
+	unsigned int	u;
+	unsigned int	next;
+
+	philo = (t_diner *)arguments;
+	u = philo->diner_id + 1;
+	next = u % philo->diet[PHILOSOPHERS] + 1;
+	
+	while (philo->forks[u - 1] && philo->forks[next - 1])
+	{
+		philo->forks[u - 1] = 0;
+		philo->forks[next - 1] = 0;
+		printf("P%u: I toke my fork and the one of P%u.\n", u, next);
+		sleep(3);
+		philo->forks[u - 1] = 1;
+		philo->forks[next - 1] = 1;
+		printf("P%u: I released my fork and the one of P%u.\n", u, next);
+		sleep(3);
+	}
+	return (NULL);
+}
