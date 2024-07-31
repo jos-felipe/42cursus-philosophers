8c28e6da49d0a67d3646c520a225b423c2ee0f2f
josfelip@debian <josfelip@debian>
2024-07-31 11:37:39 -0300

    double

--- philo/04_timestamp.c
+++ philo/04_timestamp.c
@@ -6,26 +6,24 @@
 /*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 12:06:28 by josfelip          #+#    #+#             */
-/*   Updated: 2024/07/30 16:10:50 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/07/31 11:35:47 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static long int	philo_seconds(struct timeval meal_start)
+static double	philo_timestamp_ms(struct timeval meal_start)
 {
-	long int	timestamp;
+	double			t0;
+	double			t1;
+	struct timeval	tv;
 	
-	timestamp = (long int)meal_start.tv_sec;
-	return (timestamp);
-}
-
-static long int	philo_useconds(struct timeval meal_start)
-{
-	long int	timestamp;
-	
-	timestamp = (long int)meal_start.tv_usec;
-	return (timestamp);
+	t0 = (double)meal_start.tv_sec * (double)1000;
+	t0 += (double)meal_start.tv_usec / (double)1000;
+	assert(!gettimeofday(&tv, NULL));
+	t1 = (double)tv.tv_sec * (double)1000;
+	t1 += (double)tv.tv_usec / (double)1000;
+	return (t1 - t0);
 }
 
 void			philo_timestamp_eat_sleep_think(t_diner *philo, \
@@ -33,14 +31,14 @@ unsigned int u, unsigned int next)
 {
 	philo->forks[u - 1] = 0;
 	philo->forks[next - 1] = 0;
-	printf("%ld.%ld %u has taken a fork\n", philo_seconds(philo->meal_start), philo_useconds(philo->meal_start) , u);
-	printf("%ld.%ld %u is eating\n", philo_seconds(philo->meal_start), philo_useconds(philo->meal_start) , u);
+	printf("%f %u has taken a fork\n", philo_timestamp_ms(philo->meal_start), u);
+	printf("%f %u is eating\n", philo_timestamp_ms(philo->meal_start), u);
 	usleep(philo->diet[TIME_TO_EAT] * 1000);
 	philo->forks[u - 1] = 1;
 	philo->forks[next - 1] = 1;
 	if (philo->diet[MEALS])
 		philo->diet[MEALS] -= 1;
-	printf("%ld.%ld %u is sleeping\n", philo_seconds(philo->meal_start), philo_useconds(philo->meal_start) , u);
+	printf("%f %u is sleeping\n", philo_timestamp_ms(philo->meal_start), u);
 	usleep(philo->diet[TIME_TO_SLEEP] * 1000);
-	printf("%ld.%ld %u is thinking\n", philo_seconds(philo->meal_start), philo_useconds(philo->meal_start) , u);
+	printf("%f %u is thinking\n", philo_timestamp_ms(philo->meal_start), u);
 }
